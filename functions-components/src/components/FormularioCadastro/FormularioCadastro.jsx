37a7fc15b02c7da0379c1c0ae44f4f82078d7a26
Douglas Oliveira <dougmoroli@gmail.com>
2023-06-08 17:01:40 -0300

    refatorando retirando switch case

--- functions-components/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ functions-components/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -5,26 +5,16 @@ import DadosEntrega from "./DadosEntrega";
 
 export default function FormularioCadastro({ aoEnviar, validarCPF }) {
     const [etapaAtual, setEtapaAtual] = useState(0);
+    const formularios = [
+        <DadosUsuario aoEnviar={proximo} />,
+        <DadosPessoais aoEnviar={proximo} validarCPF={validarCPF} />,
+        <DadosEntrega aoEnviar={aoEnviar} />,
+    ];
 
     //FUNÇÃO QUE AVANÇA PARA A PRÓXIMA ETAPA
     function proximo() {
         setEtapaAtual(etapaAtual + 1);
     }
 
-    //FUNÇÃO QUE PERMITE ALTERAR O FORMULÁRIO ATUAL
-    function formularioAtual(etapa) {
-        switch (etapa) {
-            case 0:
-                return <DadosUsuario aoEnviar={proximo} />;
-            case 1:
-                return (
-                    <DadosPessoais aoEnviar={proximo} validarCPF={validarCPF} />
-                );
-            case 2:
-                return <DadosEntrega aoEnviar={aoEnviar} />;
-            default:
-                return <DadosUsuario />;
-        }
-    }
-    return <Fragment>{formularioAtual(etapaAtual)}</Fragment>;
+    return <Fragment>{formularios[etapaAtual]}</Fragment>;
 }
