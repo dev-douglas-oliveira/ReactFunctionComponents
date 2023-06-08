0ec8736629dca5a0843dc23320659fe1eb912ddd
Douglas Oliveira <dougmoroli@gmail.com>
2023-06-08 17:41:36 -0300

    usando Stepper do material UI

--- functions-components/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ functions-components/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -2,19 +2,23 @@ import React, { Fragment, useEffect, useState } from "react";
 import DadosPessoais from "./DadosPessoais";
 import DadosUsuario from "./DadosUsuario";
 import DadosEntrega from "./DadosEntrega";
+import { Step, StepLabel, Stepper, Typography } from "@mui/material";
 
 export default function FormularioCadastro({ aoEnviar, validarCPF }) {
     const [etapaAtual, setEtapaAtual] = useState(0);
     const [dadosColetados, setDados] = useState({});
 
     useEffect(() => {
-        console.log(dadosColetados);
+        if (etapaAtual === formularios.length - 1) {
+            aoEnviar(dadosColetados);
+        }
     });
 
     const formularios = [
         <DadosUsuario aoEnviar={coletarDados} />,
         <DadosPessoais aoEnviar={coletarDados} validarCPF={validarCPF} />,
         <DadosEntrega aoEnviar={coletarDados} />,
+        <Typography variant="h5">Cadastro efetuado com sucesso </Typography>,
     ];
 
     //FUNÇÃO PARA COLETAR DADOS
@@ -28,5 +32,23 @@ export default function FormularioCadastro({ aoEnviar, validarCPF }) {
         setEtapaAtual(etapaAtual + 1);
     }
 
-    return <Fragment>{formularios[etapaAtual]}</Fragment>;
+    return (
+        <Fragment>
+            <Stepper activeStep={etapaAtual}>
+                <Step>
+                    <StepLabel>Login</StepLabel>
+                </Step>
+                <Step>
+                    <StepLabel>Pessoal</StepLabel>
+                </Step>
+                <Step>
+                    <StepLabel>Entrega</StepLabel>
+                </Step>
+                <Step>
+                    <StepLabel>Finalização</StepLabel>
+                </Step>
+            </Stepper>
+            {formularios[etapaAtual]}
+        </Fragment>
+    );
 }
